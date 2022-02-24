b03e7e03a448a7bdd5e89ed1cca15a0f74347303
AnnaAngel3 <85314783+AnnaAngel3@users.noreply.github.com>
2022-02-24 16:28:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function setup(){
+     canvas=createCanvas(280,280)
+     canvas.center()
+     background("white")
+
+    }
+
+    function clearCanvas(){
+        background("white")
+        
+    }
